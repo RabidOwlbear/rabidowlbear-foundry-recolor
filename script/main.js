bc71f30e4b2fe26393731875afc0bbb5114e33b5
Grim <owlbearaviary@gmail.com>
2021-07-07 22:40:45 -0700

    fixed link

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 //change foundry logo
 const logo = document.querySelector('#logo');
-logo.src = 'modules/rabidowlbear-foundry-recolor/image/fvtt-logo-a.png';
+logo.src = 'modules/rabidowlbear-foundry-recolor/image/fvtt-logo-a.webp';
 console.log('logo', logo);
 
 Hooks.on('renderPause', (app, html, options) => {
