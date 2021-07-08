01b24d008eceb64ea1a3d703c0e1e3b6204a415e
Grim <owlbearaviary@gmail.com>
2021-07-07 20:22:57 -0700

    added pause image

--- script/main.js
+++ script/main.js
@@ -2,3 +2,6 @@
 const logo = document.querySelector('#logo');
 logo.src = 'modules/rabidowlbear-foundry-recolor/image/fvtt-pink.png';
 console.log('logo', logo);
+const pauseImg = document.querySelector('#pause img');
+console.log('pauseImg', pauseImg);
+pauseImg.src = 'modules/rabidowlbear-foundry-recolor/image/skele-pause.webp';
