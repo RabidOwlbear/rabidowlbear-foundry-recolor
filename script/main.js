212e9ae8ac10fdc24670f0f5aab60102d03e95a1
Grim <71675732+RabidOwlbear@users.noreply.github.com>
2021-07-07 19:40:45 -0700

    updated file path

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
 //change foundry logo
 const logo = document.querySelector('#logo');
-logo.src = '/modules/my-fvtt-recolor/image/fvtt-pink.png';
+logo.src = './image/fvtt-pink.png';
 console.log('logo', logo);
